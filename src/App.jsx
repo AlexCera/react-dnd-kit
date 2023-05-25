0b7f5fa0cc6793e28f3b5b74c9484ad909d5d0a7
= <=>
2023-05-25 17:49:14 -0500

    Set up tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { DndContext, closestCenter } from "@dnd-kit/core"
+import { useState } from "react"
+
 function App() {
+
+  const [people, setPeople] = useState([
+    { id: 1, name: "Juan" },
+    { id: 10, name: "Jose" },
+    { id: 100, name: "Jorge" },
+    { id: 1000, name: "Jazmin" },
+    { id: 10000, name: "Jesus" },
+  ])
+
+
+  /* Manejar evento cuando se suelte un elemento */
+  const handleDragEnd = () => {
+
+  }
+
   return (
-    <div>
-      Hello vite app
-    </div>
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+
+      <h1 className="text-2xl font-bold text-red-500">Contacts</h1>
+
+    </DndContext>
   )
 }
 
